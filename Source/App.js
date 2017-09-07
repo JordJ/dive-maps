ea33b7124a0c8e8f6de0bffa8bf6651b57ad3a2e
Jordan Johnson <jordan@jordanjohnson.com.au>
2017-09-07 06:39:46 +0000

    Base Cesium application (untested)

---
+++ Source/App.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        scene3DOnly: true,
+        selectionIndicator: false,
+        baseLayerPicker: false,
+        animation: false,
+        timeline: false
+    });
+
+}());
